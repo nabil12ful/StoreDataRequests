c2c753dd7f923b1fe4ff7dd4ad63fe6ba705edfd
Nabil Hamada <31300818+nabil12ful@users.noreply.github.com>
2023-01-28 15:12:07 +0200

    v1.0

---
+++ src/StoreDataRequestsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Nabil\StoreDataRequests;
+
+use Illuminate\Support\ServiceProvider;
+
+class StoreDataRequests extends ServiceProvider
+{
+    public function boot()
+    {
+        // 
+    }
+    public function register()
+    {
+        // 
+    }
+}
